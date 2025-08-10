3dd052ddb1988469b3c5f4a16af2ea462eec28e9
Ricardo Brown <57880227+Server101@users.noreply.github.com>
2025-08-10 13:47:10 -0400

    8.1

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -158,7 +158,7 @@ const fmtDuration = (seconds, withSeconds = false) => {
         className="parallax-window tm-section tm-section-home d-flex align-items-center"
         id="home"
         data-parallax="scroll"
-        data-image-src="/img/ricardoTech_0212.jpg"
+        data-image-src="/img/ricardo2.jpg"
         style={{ minHeight: '100vh' }}
       >
         <div className="container">
